97668b4fb1455b212030ccedd9c38c6b1ae8d2e4
nahom zenebe <nahomzenebe123@gmail.com>
2025-01-09 16:47:46 +0300

    add the seller page

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -10,7 +10,8 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin: 'http://127.0.0.1:5500',
+    origin: [ 'http://127.0.0.1:5500', 
+      'http://127.0.0.1:62213']
   });
 
   await app.listen(process.env.PORT ?? 3000);
