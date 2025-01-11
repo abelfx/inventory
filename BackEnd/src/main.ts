8ad895eea808ac7baa1161dc2eb26857515ab5c3
nahom zenebe <nahomzenebe123@gmail.com>
2025-01-11 16:20:58 +0300

    fixing the account page

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableCors({
     credentials: true,
     origin: [ 'http://127.0.0.1:5500', 
-      'http://127.0.0.1:62213','http://127.0.0.1:54505']
+      'http://127.0.0.1:62213','http://127.0.0.1:54505','http://127.0.0.1:64832/']
   });
 
   await app.listen(process.env.PORT ?? 3000);
